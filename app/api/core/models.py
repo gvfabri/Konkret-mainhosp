826c79080ab3eb90fe00196dbafcd87559d0eaf8
gus-chi <gchiato@unifesp.br>
2024-10-30 21:23:02 -0300

    modelagem-proprietarios

--- app/api/core/models.py
+++ app/api/core/models.py
@@ -15,7 +15,12 @@ class User(Base):
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
+class Proprietary(Base):
+    __tablename__ = "proprietaries"
 
-
-
+    id = Column(String, primary_key=True, index=True,default=lambda: str(uuid4()))
+    name = Column(String, nullable=False)
+    cpf = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
