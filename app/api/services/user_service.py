65d6274855e1cf3f5bd9e2ea48aa4a04bf0c93d6
gfabri <gabiel_fabri13@outlook.com>
2024-11-10 18:19:08 -0300

    Remodelando tabelas users

--- app/api/services/user_service.py
+++ app/api/services/user_service.py
@@ -5,11 +5,11 @@ class UserService:
     def __init__(self, db: Session):
         self.user_repository = UserRepository(db)
 
-    def create_user(self,name: str,phone: str,email: str):
-        self.user_repository.create(name,phone,email)
+    def create_user(self,name: str,phone: str,email: str, password: str):
+        self.user_repository.create(name,phone,email, password)
 
-    def update(self, id: str, phone: str = None, email: str = None):
-        self.user_repository.update(id, phone, email)
+    def update(self, id: str, phone: str = None, email: str = None, password: str = None):
+        self.user_repository.update(id, phone, email, password)
 
     def all(self):
         return self.user_repository.all()
